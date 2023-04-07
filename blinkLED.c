ad99198c97c4fe08838aa5e8ca0a7c7331680a2d
Babak Jahedmanesh <babak.jahedmanesh@thenavpeople.co.uk>
2023-04-07 01:32:17 +0100

    crazy order w/ D7 and D6

--- blinkLED.c
+++ blinkLED.c
@@ -1,38 +1,45 @@
-                                                       /* Blinker Demo */
+/* Blinker Demo */
 
 // ------- Preamble -------- //
-#include <avr/io.h>                        /* Defines pins, ports, etc */
-#include <util/delay.h>                     /* Functions to waste time */
+#include <avr/io.h>     /* Defines pins, ports, etc */
+#include <util/delay.h> /* Functions to waste time */
 
-
-int main(void) {
+int main(void)
+{
 
   // -------- Inits --------- //
-  DDRB |= 0b00000001;            /* Data Direction Register B:
-                                   writing a one to the bit
-                                   enables output. */
+  DDRB |= 0b00000001; /* Data Direction Register B:
+                        writing a one to the bit
+                        enables output. */
+  DDRD |= 0b11000000;
 
   // ------ Event loop ------ //
-  while (1) {
+  while (1)
+  {
 
-    PORTB = 0b00000001;          
-    _delay_ms(100);                                           
+    PORTD = 0b01000000;
+    PORTB = 0b00000001;
+    _delay_ms(100);
 
-    PORTB = 0b00000000;          
-    _delay_ms(100);     
+    PORTD = 0b11000000;
+    PORTB = 0b00000000;
+    _delay_ms(100);
 
-        PORTB = 0b00000001;          
-    _delay_ms(100);                                           
+    PORTD = 0b01000000;
+    PORTB = 0b00000001;
+    _delay_ms(100);
 
-    PORTB = 0b00000000;          
-    _delay_ms(100);  
+    PORTD = 0b10000000;
+    PORTB = 0b00000000;
+    _delay_ms(100);
 
-        PORTB = 0b00000001;          
-    _delay_ms(100);                                           
+    PORTD = 0b00000000;
+    PORTB = 0b00000001;
+    _delay_ms(100);
 
-    PORTB = 0b00000000;          
-    _delay_ms(1000);                                        
-
-  }                                                 
-  return 0;                            
+    PORTB = 0b00000000;
+    PORTD = 0b10000000;
+    _delay_ms(1000);
+  }
+  return 0;
 }
